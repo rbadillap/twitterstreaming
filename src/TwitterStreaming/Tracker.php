42d277ccf7056837a8e827b8ab0f019245989b7c
Ronny Badilla <rbadillap@gmail.com>
2016-01-04 08:38:43 -0600

    Update the application version and fix an issue with the filter module

--- src/TwitterStreaming/Tracker.php
+++ src/TwitterStreaming/Tracker.php
@@ -6,7 +6,7 @@
  * PHP library to connect to the Twitter Streaming API
  * and retrieve data in real-time.
  *
- * @version 0.1.2
+ * @version 0.1.3
  * @license MIT
  */
 namespace TwitterStreaming;
